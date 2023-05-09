1b6700643c8f4ed39e5c1221b5d481b88a5cfca2
SangryeolPark <tkdfuf0923@gmail.com>
2023-05-09 13:02:32 +0900

    fix: Footer 스타일 수정 및 일부 이미지 파일 경로 변경 feat: Side 작업 중

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+let countries = ["대한민국 / KRW", "USA / USD", "日本 / JPY", "中国 / CNY", "España / EUR", "Deutschland / EUR", "Indonesia / IDR", "ประเทศไทย / THB", "La France / EUR", "Portugal / BRL", "Australia / AUD", "Canada / CAD", "香港 / HKD", "Malaysia / MYR", "New Zealand / NZD", "Pilipinas / PHP", "Singapore / SGD", "台灣 / TWD", "United Kingdom / GBP", "Việt Nam / VND", "Россия / RUB"];
+
 document.addEventListener("DOMContentLoaded", () => {
     let goTop = document.querySelector(".go-top");
     let currentScroll = window.scrollY;
